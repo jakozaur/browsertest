9b55db5b7be8a2c1bd5e7baf0e6165de14cfd6d4
Jacek Migdal <jacek@migdal.pl>
2014-12-28 15:31:53 -0800

    Add Code Mirror

---
+++ client/code.js
@@ -0,0 +1,25 @@
+Template.code.helpers({
+  options: function () {
+    return {
+      mode: 'javascript',
+      lineNumbers: true
+    };
+  },
+  exampleCode: function () {
+    return "\
+module.exports = {\n\
+  \"Demo test Google\" : function (browser) {\n\
+    browser\n\
+      .url('http://www.google.com')\n\
+      .waitForElementVisible('body', 1000)\n\
+      .setValue('input[type=text]', 'nightwatch')\n\
+      .waitForElementVisible('button[name=btnG]', 1000)\n\
+      .click('button[name=btnG]')\n\
+      .pause()\n\
+      .assert.containsText('#main', 'The Night Watch')\n\
+      .end();\n\
+  }\n\
+};\n\
+";
+  }
+});
